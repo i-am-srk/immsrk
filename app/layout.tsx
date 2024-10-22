96c2afe753f1594df81ac10b7634e13c2ee4a2aa
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2024-10-22 11:14:30 +0530

    added bio

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         {/* blue background */}
         <div
