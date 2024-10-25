584cff214ab373849ca88822ceb67c8d4a56dbcc
i-am-srk <shah.rukh.khan.IITian@gmail.com>
2024-10-25 12:03:04 +0530

    intro completed

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithub } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -60,6 +64,66 @@ export default function Intro() {
       </motion.p>
 
       {/* contact info buttons */}
+      <motion.div
+        className="flex flex-col sm:flex-row 
+        items-center justify-center text-lg
+        gap-2 px-4 font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 
+          text-white px-7 py-3 flex
+           items-center rounded-full gap-2
+           outline-none focus:scale-110
+           hover:scale-110 hover:bg-gray-950
+           active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
+        </Link>
+
+        <a
+          className="group bg-white px-7
+          py-3 gap-2 flex items-center
+          rounded-full outline-none
+          focus:scale-110 hover:scale-110
+          active:scale-105 transition
+          cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700
+          hover:text-gray-950 gap-2 flex items-center
+          rounded-full focus:scale-[1.15] hover:scale-[1.15]
+          active:scale-105 transition cursor-pointer border
+          border-black/10"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700
+          gap-2 flex items-center rounded-full 
+          text-[1.35rem] focus:scale-[1.15]
+          hover:scale-[1.15] hover:text-gray-950
+          active:scale-105 transition
+          cursor-pointer border border-black/10"
+          href="https://github.com"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+      </motion.div>
     </section>
   );
 }
